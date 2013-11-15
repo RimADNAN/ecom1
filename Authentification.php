bfe4b51e5777b00fc05a23b6d1ae7477f62c997d
Rim Adnan <rim.adnan@gmail.com>
2013-11-15 21:56:50 +0000

    Create Authentification.php

---
+++ Authentification.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<title>Authentification</title>
+
+<center><form action="auth.php" method="POST">
+		<table>
+		<tr><td>Login :</font></td><td><input type="text" name="log" class="zonetext " required="required"></td>
+		 </tr>
+		<tr><td>Password :</font></td><td><input type="password" required="required" name="pwd" class="zonetext " ></td> 
+		</tr>
+		<td></td>
+		<td colspan="2"><input type="submit" name="Envoyer" value="Se connecter"/></td>
+		</tr>
+		</table></form>		
+</center>
+
+<body>
+</html>
