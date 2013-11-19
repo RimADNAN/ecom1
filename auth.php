83b5f09ea79e588166cb26a233913427a3f70aac
Rim Adnan <rim.adnan@gmail.com>
2013-11-19 20:04:23 +0000

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,35 +1,53 @@
 <?php
 
-if($_POST['Envoyer'])
-{
-	if($_POST["log"] != "" && $_POST["pwd"] != "")
-	{
-		  $user = $_POST["log"] ;
-		  $pass  = $_POST["pwd"] ;
-			include("connection.php");
-		  $sql = "SELECT statut FROM user WHERE login = '".$user."' AND pwd = '".$pass."'"	;
-		  $requete = mysql_query($sql) or die($sql."<br>".mysql_error()) ;
-		  
-		  //on récupère le résultat
-		 $result = mysql_fetch_object($requete) ;
-		  
-		  //si la requête s'est bien passée
-		  if(is_object($result))
-		  {
-				session_start() ;
-				$_SESSION["statut"] = $sql;
-				
-				//user
-				if( $sql = 0 ) { header("Location: profil_u.php") ;}
-				
-				//admin
-			    else { header("Location: profil_a.php") ;}
-		  }
-		  //auth incorrecte
-		  else  {	header("Location: index.php") ; }
+session_start();
+include("connection.php");
+
+class Authentification{
+
+   static function Test_auth()
+   {   if($_POST['Envoyer'])
+			{ if($_POST["log"] != "" && $_POST["pwd"] != "")
+				{ 
+				$user = $_POST["log"] ;
+				$pass  = $_POST["pwd"] ;
+				$sql = "SELECT statut FROM user WHERE login_user = '".$user."' AND pwd_user = '".$pass."'" ;
+				$req = mysql_query($sql) or die(mysql_error());
+				if(mysql_num_rows($req) > 0){ return true;	}
+				return false;
+			    }
+			  return false;
+		     }
 	}
-	//log et pwd vide
-	else	{  header("Location: index.php") ;}
+	
+	static function Statut()
+   {   if($_POST['Envoyer'])
+			{ if($_POST["log"] != "" && $_POST["pwd"] != "")
+				{ 
+				$user = $_POST["log"] ;
+				$pass  = $_POST["pwd"] ;
+				$sql = "SELECT statut FROM user WHERE login_user = '".$user."' AND pwd_user = '".$pass."'" ;
+				$req = mysql_query($sql) or die(mysql_error());
+				if(mysql_num_rows($req) > 0){ $st = mysql_fetch_assoc($req); 	
+											  return $st['statut'];
+											}
+				return false;
+			    }
+			  return false;
+		     }
+	}
+		   
 }
 
+if(Authentification::Test_auth())
+{
+   switch(Authentification::Statut()){               
+             case "1":
+                header("Location: profil_a.php") ;;
+                break;
+             case "2":
+                header("Location: profil_u.php") ;;
+                break;
+			}
+	}else{ header("Location: index.php") ;}
 ?>
